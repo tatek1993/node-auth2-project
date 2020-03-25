585cb55c31ebbcb1e9a0fb75d17321b20a7f2a68
Tate Keller <tatek1993@gmail.com>
2020-03-24 20:43:52 -0500

    initial commit, and second commit, set up file structure, installed dependencies, set up server, routers and configs

---
+++ auth/authRouter.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/usersModel.js');
+// import secrets
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = makeToken(user);
+                res.status(200).json({
+                    message: `Good to see ya, ${user.username}!`,
+                    token,
+                });
+            } else {
+                res.status(401).json({ message: "Those credentials are wrong!" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+function makeToken(user) {
+    const payload = {
+        
+    }
+}
+
